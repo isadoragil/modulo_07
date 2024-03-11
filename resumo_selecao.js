672c485fcfb8be148bf1c1b6f9f7129bcda2ce1a
Isadora Gil de Souza <105384553+isadoragil@users.noreply.github.com>
2024-03-11 13:24:47 -0300

    2.0

---
+++ resumo_selecao.js
@@ -0,0 +1,12 @@
+document.getElementsByTagName('input')
+document.getElementById('nome-beneficiario')
+document.getElementsByTagName('form')[0].getElementsByTagName('button')
+document.getElementsByClassName('container')
+document.querySelector('button') //retorna primeiro item
+document.querySelector('input[type=number]') 
+document.querySelectorAll('input[type=number]') //retorna todos os tipos numero
+document.querySelector('input#numero-conta')
+document.querySelector('h1')
+if (document.querySelector('h3')) //caso não tenha o item
+document.body
+document.head

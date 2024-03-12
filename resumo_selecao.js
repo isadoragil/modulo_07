4eba06c7e368cd844d10345ea0a93cd12eab2074
Isadora Gil de Souza <105384553+isadoragil@users.noreply.github.com>
2024-03-12 13:17:38 -0300

    3.0

--- resumo_selecao.js
+++ resumo_selecao.js
@@ -10,3 +10,16 @@ document.querySelector('h1')
 if (document.querySelector('h3')) //caso não tenha o item
 document.body
 document.head
+
+/*-------------------------------------------------------------------------------*/
+
+let nomeBeneficiario = document.getElementById('nome-beneficiario')
+nomeBeneficiario //retorna o valor do html
+nomeBeneficiario.value //retorna o valor da variável
+nomeBeneficiario.value = 'Isadora Gil'
+nomeBeneficiario.type //tipo da variável
+nomeBeneficiario.id //retorna o id do campo no html
+nomeBeneficiario.required // se é obrigatório = true ou false
+nomeBeneficiario.placeholder
+
+
